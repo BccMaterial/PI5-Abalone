b2ca7f87796cdd7f82562af2bcb0c6ee616bc1ed
thiagopls1 <thiagopls1@hotmail.com>
2025-03-14 12:20:51 -0300

    Remove `camelCase` and implement `imprimir_tabuleiro`

--- classes/abalone.py
+++ classes/abalone.py
@@ -1,38 +1,70 @@
 class Abalone:
-    def init(self):
-        self.tabuleiro = self.criarTabuleiro() #Cria o Tabuleiro
-        self.turnoJogador= 1 #Define se é o turno do jogador 1 ou 2
-        self.pecasDerrubadas= {1: 0, 2: 0} #1 se refere ao primeiro jogador e quantas peças ele perdeu. 2 se refere ao segundo jogador e quantas pecas ele perdeu
-    def criarTabuleiro(self): #1 e 2 são utilizados para representar os espaços ocupados pelas peças dos jogadores e None é utilizado para representar os espaços não ocupados
-        return [  #Baseado em relação a este tabuleiro: https://www.divertivida.com.br/abalone-classic
+    def __init__(self):
+        self.tabuleiro = self.criar_tabuleiro()
+        self.turno_jogador= 1
+        # 1 -> Pontuação primeiro jogador
+        # 2 -> Pontuação segundo jogador
+        self.pecas_derrubadas= {
+            1: 0, 
+            2: 0
+        }
+
+    # 1 e 2 são utilizados para representar os espaços ocupados pelas peças 
+    # dos jogadores e None é utilizado para representar os espaços não ocupados
+    def criar_tabuleiro(self):
+        # Baseado em relação a este tabuleiro: https://www.divertivida.com.br/abalone-classic
+        # Representação do tabuleiro:
+        #
+        #     2 2 2 2 2
+        #    2 2 2 2 2 2
+        #   - - 2 2 2 - -
+        #  - - - - - - - -
+        # - - - - - - - - -
+        #  - - - - - - - -
+        #   - - 1 1 1 - - 
+        #    1 1 1 1 1 1
+        #     1 1 1 1 1
+        #
+        return [
             [1, 1, 1, 1, 1],
             [1, 1, 1, 1, 1, 1],
-            [None, None, 1, 1, 1, None, None],
-            [None, None, None, None, None, None, None, None],
-            [None, None, None, None, None, None, None, None, None],
-            [None, None, None, None, None, None, None, None],
-            [None, None, 2, 2, 2, None, None],
+            [0, 0, 1, 1, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 2, 2, 2, 0, 0],
             [2, 2, 2, 2, 2, 2],
             [2, 2, 2, 2, 2,],
         ]
 
-    def movimentarPecas(self, posicaoJogador, direcao):
+    def movimentar_pecas(self, posicao_jogador, direcao):
+        # Implementar função
+        pass
+
+    def empurrar_oponente(self):
         #Implementar função
         pass
 
-    def empurrarOponente(self):
-        #Implementar função
-        pass
+    def checar_vitoria(self):
+        # Checa os valores associados as Keys do dicionário, 
+        # e caso qualquer um deles seja maior ou igual a 6, temos um vencedor
+        return self.pecas_derrubadas[1] >= 6 or self.pecas_derrubadas[2]>=6
 
-    def checarVitoria(self):
-        return self.pecasDerrubadas[1] >= 6 or self.pecasDerrubadas[2]>=6 #Checa os valores associados as Keys do dicionário, e, caso qualquer um deles seja maior ou igual a 6, temos um vencedor
-
-    def imprimirTabuleiro(self): #Implementar
+    def imprimir_tabuleiro(self):
+        tamanho_maximo = len(max(self.tabuleiro, key=len))
+        str_final = ""
+        str_final += "\n"
         for linha in self.tabuleiro:
-            print() #Se o elemento do array for 1 ou 2, transformar o mesmo em uma string, concatena-los e printar na tela. Caso o elemento do array esteja vazio, fazer o mesmo utilizando um "." para representar o vazio no tabuleiro
+            quantidade_espaços = tamanho_maximo - len(linha)
+            str_final += "".join(" " for x in range(quantidade_espaços))
+            str_final += " ".join([str(i) for i in linha])
+            str_final += "\n"
+        print(str_final)
 
     def jogar(self):
-        self.imprimirTabuleiro()
-        print(f"Turno do jogador {self.turnoJogador}")
-        #implementar
-        self.turnoJogador = 3 -self.turnoJogador #Utilizado para alternar entre o player 1 e 2. Também temos que determinar a maneira de escolher quem será o jogador 1 e 2
+        self.imprimir_tabuleiro()
+        print(f"Turno do jogador {self.turno_jogador}")
+        # TODO: Implementar
+        # Utilizado para alternar entre o player 1 e 2. Também temos que determinar 
+        # a maneira de escolher quem será o jogador 1 e 2
+        self.turno_jogador = 3 - self.turno_jogador
