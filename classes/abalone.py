7705490cdc308bfd5af75fb35ba4b1fc3fe65d12
thiagopls1 <thiagopls1@hotmail.com>
2025-03-14 21:34:09 -0300

    Refactor `movimentar_peca`

--- classes/abalone.py
+++ classes/abalone.py
@@ -27,15 +27,15 @@ class Abalone:
         #
         # O -1 é pra compensar os tamanhos das listas (vai facilitar na hora de mover)
         return [
-            [-1, -1, -1, -1, 1, 1, 1, 1, 1],
-            [-1, -1, -1, 1, 1, 1, 1, 1, 1],
-            [-1, -1, 0, 0, 1, 1, 1, 0, 0],
-            [-1, 0, 0, 0, 0, 0, 0, 0, 0],
+            [1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1],
+            [0, 0, 1, 1, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [-1, 0, 0, 0, 0, 0, 0, 0, 0],
-            [-1, -1, 0, 0, 2, 2, 2, 0, 0],
-            [-1, -1, -1, 2, 2, 2, 2, 2, 2],
-            [-1, -1, -1, -1, 2, 2, 2, 2, 2,],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 2, 2, 2, 0, 0],
+            [2, 2, 2, 2, 2, 2],
+            [2, 2, 2, 2, 2,],
         ]
 
     # Get e set compatível com tuplas
@@ -50,157 +50,82 @@ class Abalone:
     def movimentar_peca(self, peca_pos: tuple, direcao: str):
         """
         Observações:
-        - peca_jogador é formada por: (linha, coluna)
+        - peca_pos é formada por: (linha, coluna)
+        """        
 
-        Movimentos:
-        - e -> (linha, coluna-1)
-        - d -> (linha, coluna+1)
-        - ce -> (linha-1, coluna-1)
-        - cd -> (linha-1, coluna)
-        - bd -> (linha+1, coluna+1)
-        - be -> (linha+1, coluna)        
-        """
+        pos_atual = peca_pos
+        proxima_pos = self.calcular_pos(pos_atual, direcao.lower())
         
-        ########################
-        ### FUNÇÃO VALIDAÇÃO ###
-        ########################
+        if proxima_pos is None:
+            # print("Posição selecionada inválida!")
+            return
 
-        def posicao_valida(pos: tuple):
-            max_linha = len(self.tabuleiro) - 1
-            # Se -1 -1 -1 0 0 2 2 0 0
-            # Pos -> 3-8
-            linha_escolhida = self.tabuleiro[pos[0]]
-            max_coluna = len([x for x in linha_escolhida]) - 1
-            min_coluna = len([x for x in linha_escolhida if x == -1]) + 1
-            valor_pos = self.get_tabuleiro(pos)
-            return \
-                valor_pos == 0 and \
-                pos[0] <= max_linha and \
-                pos[1] <= max_coluna and \
-                pos[1] >= min_coluna
+        self.set_tabuleiro(proxima_pos, self.get_tabuleiro(pos_atual))
+        self.set_tabuleiro(pos_atual, 0)
 
-        ################
-        ### DIREÇÕES ###
-        ################
-        # Obs: Não me pergunte por que os + e - dos movimentos,
-        # Só sei que fui testando e funcionou
-        # e -> (linha, coluna-1)
-        # d -> (linha, coluna+1)
-        # ce -> (linha-1, coluna-1)
-        # cd -> (linha-1, coluna)
-        # bd -> (linha+1, coluna+1)
-        # be -> (linha+1, coluna)
+    def posicao_valida(self, pos: tuple):
+        max_linha = len(self.tabuleiro) - 1
+        # Se -1 -1 -1 0 0 2 2 0 0
+        # Pos -> 3-8
+        linha_escolhida = self.tabuleiro[pos[0]]
+        max_coluna = len([x for x in linha_escolhida]) - 1
+        min_coluna = len([x for x in linha_escolhida if x == -1]) + 1
+        valor_pos = self.get_tabuleiro(pos)
+        return \
+            valor_pos == 0 and \
+            pos[0] <= max_linha and \
+            pos[1] <= max_coluna and \
+            pos[1] >= min_coluna
 
-        def esquerda():
-            pos_atual = peca_pos
-            proxima_pos = (peca_pos[0], peca_pos[1]-1)
+    def calcular_pos(self, pos: tuple, direcao: str):
+        """
+        Função para pegar a posição seguinte com base na posição e na direção inserida.
 
-            if not posicao_valida(proxima_pos):
-                return
-
-            self.set_tabuleiro(proxima_pos, self.get_tabuleiro(pos_atual))
-            self.set_tabuleiro(pos_atual, 0)
-
-        def direita():
-            pos_atual = peca_pos
-            proxima_pos = (peca_pos[0], peca_pos[1]+1)
-
-            if not posicao_valida(proxima_pos):
-                return
-
-            self.set_tabuleiro(proxima_pos, self.get_tabuleiro(pos_atual))
-            self.set_tabuleiro(pos_atual, 0)
-
-        def cima_esquerda():
-            pos_atual = peca_pos
-            proxima_pos = (peca_pos[0]-1, peca_pos[1]-1)
-            print(f"{pos_atual} -> {proxima_pos}")
-
-            if not posicao_valida(proxima_pos):
-                return
-
-            self.set_tabuleiro(proxima_pos, self.get_tabuleiro(pos_atual))
-            self.set_tabuleiro(pos_atual, 0)
-
-        def cima_direita():
-            pos_atual = peca_pos
-            proxima_pos = (peca_pos[0]-1, peca_pos[1])
-
-            if not posicao_valida(proxima_pos):
-                return
-
-            self.set_tabuleiro(proxima_pos, self.get_tabuleiro(pos_atual))
-            self.set_tabuleiro(pos_atual, 0)
-
-        def baixo_direita():
-            pos_atual = peca_pos
-            proxima_pos = (peca_pos[0]+1, peca_pos[1]+1)
-
-            if not posicao_valida(proxima_pos):
-                return
-
-            self.set_tabuleiro(proxima_pos, self.get_tabuleiro(pos_atual))
-            self.set_tabuleiro(pos_atual, 0)
-
-        def baixo_esquerda():
-            pos_atual = peca_pos
-            proxima_pos = (peca_pos[0]+1, peca_pos[1])
-            
-            if not posicao_valida(proxima_pos):
-                return
-
-            self.set_tabuleiro(proxima_pos, self.get_tabuleiro(pos_atual))
-            self.set_tabuleiro(pos_atual, 0)
-
-        #####################
-        ### IMPLEMENTAÇÃO ###
-        #####################
+        > Retornará `None` caso seja uma posição inválida
+        """
 
         movimentos = {
-            "e": esquerda,
-            "d": direita,
-            "ce": cima_esquerda,
-            "cd": cima_direita,
-            "be": baixo_esquerda,
-            "bd": baixo_direita
+            "e": (pos[0], pos[1] - 1), # Continua na linha, volta uma coluna
+            "d": (pos[0], pos[1] + 1), # Continua na mesma linha, anda uma coluna
+            "ce": (pos[0] - 1, pos[1] - 1), # Diminui uma linha, volta uma coluna
+            "cd": (pos[0] - 1, pos[1] + 1), # Diminui uma linha, anda uma coluna
+            "be": (pos[0] + 1, pos[1] - 1), # Aumenta uma linha, volta uma coluna
+            "bd": (pos[0] + 1, pos[1] + 1) # Aumenta uma linha, anda uma coluna
         }
         
-        movimentos[direcao.lower()]()
+        if not self.posicao_valida(movimentos[direcao]):
+            return None
+
+        return movimentos[direcao]
 
     def empurrar_oponente(self, pecas: list, direcao: str):
 
-        #sumito: Para poder empurrar uma peça do adversário, deve se assumir uma posição de "sumito"
-        #Para que se possa entrar em sumito, a sua quantidade de esferas deve ser maior do que a do seu oponente
-        #A sua quantidade de esferas para entrar em sumito deve ser 2 ou 3
-        #Exemplo: é necessário que existe duas esferas alinhadas suas para poder empurrar uma peça adversária para tras
+        # Sumito: Para poder empurrar uma peça do adversário, deve se assumir uma posição de "sumito"
+        # Para que se possa entrar em sumito, a sua quantidade de esferas deve ser maior do que a do seu oponente
+        # A sua quantidade de esferas para entrar em sumito deve ser 2 ou 3
+        # Exemplo: é necessário que existe duas esferas alinhadas suas para poder empurrar uma peça adversária para tras
 
-        if(len(pecas) > 3 or len(pecas) < 2): #Para que o sumito possa ocorrer, é necessário que o jogador tenha 2 ou 3 peças alinhadas
+        # Para que o sumito possa ocorrer, é necessário que o jogador tenha 2 ou 3 peças alinhadas
+        if(len(pecas) >= 3 and len(pecas) <= 2): 
             print("Precisa de 2 ou 3 peças alinhadas")
             return False
 
-        def prox_pos(pos): #pos deve ser uma tupla com (linha, coluna)
-            movimentos = {
-                "e": (pos[0], pos[1] - 1), #Continua na linha, volta uma coluna
-                "d": (pos[0], pos[1] +1), #Continua na mesma linha, anda uma coluna
-                "ce": (pos[0] - 1, pos[1] -1), #Diminui uma linha, volta uma coluna
-                "cd": (pos[0] - 1, pos[1] + 1), #Diminui uma linha, anda uma coluna
-                "be": (pos[0] + 1, pos[1] - 1), #Aumenta uma linha, volta uma coluna
-                "bd": (pos[0] + 1, pos[1] + 1) #Aumenta uma linha, anda uma coluna
-            }
-            return movimentos[direcao] #Retorna uma posição a partir da posição passada
+        # Encontra a posição atual do jogador
+        pos_jogador = self.get_tabuleiro(pecas[0]) 
 
-        pos_jogador = self.get_tabuleiro(pecas[0]) #Encontra a posição atual do jogar
-        pos_oponente = prox_pos(pecas[-1]) #Tupla da peça do oponente mais próxima
+        # Tupla da peça do oponente mais próxima
+        pos_oponente = self.calcular_pos(pecas[-1], direcao)
 
         if not (0 <= pos_oponente[0] < len(self.tabuleiro) and 0 <= pos_oponente[1] < len(self.tabuleiro[pos_oponente[0]])): #Verifica se a linha do oponente é valida e se o valor da coluna do oponente é menor que o valor da linha do mesmo
             print("Sem peças oponentes nesta direção para empurrar")
             return False
 
-        if self.get_tabuleiro(pos_oponente) == 0 or self.get_tabuleiro(pos_oponente) == pos_jogador: #Verifica se existe uma peça para empurrar a frente
+        # Verifica se existe uma peça para empurrar a frente
+        if self.get_tabuleiro(pos_oponente) == 0 or self.get_tabuleiro(pos_oponente) == pos_jogador: 
             print("Não existe nenhuma peça do oponente a frente para empurrar")
 
-    def checar_vitoria(self):
-        return self.pecas_derrubadas[1] >= 6 or self.pecas_derrubadas[2]>=6
+    def testar_objetivo(self):
+        return self.pecas_derrubadas[1] >= 6 or self.pecas_derrubadas[2] >= 6
 
     def imprimir_tabuleiro(self):
         tamanho_maximo = len(max(self.tabuleiro, key=len))
@@ -208,19 +133,54 @@ class Abalone:
         str_final += f" \t{" ".join(str(i) for i in range(tamanho_maximo))}\n"
         str_final += "\n"
         for i, linha in enumerate(self.tabuleiro):
-            linha_limpa = [x for x in linha if x != -1]
-            quantidade_espaços = tamanho_maximo - len(linha_limpa)
+            quantidade_espaços = tamanho_maximo - len(linha)
             str_final += f"{i}\t"
             str_final += "".join(" " for _ in range(quantidade_espaços))
-            str_final += " ".join([str(i) for i in linha_limpa])
+            str_final += " ".join([str(i) for i in linha])
             str_final += "\n"
         print(str_final)
 
     def jogar(self):
         # O usuário vai inserir as coordenadas da peça. ("Linha, Coluna") Ex: 0, 3
         # Temos que pegar a diferença entre a lista com maior tamanho e a lista que ele escolheu
-        self.imprimir_tabuleiro()
-        print(f"Turno do jogador {self.turno_jogador}")
+        ultimo_input = ""
+        while not self.testar_objetivo():
+            self.imprimir_tabuleiro()
+            print("Instruções")
+            print("----------------------------------")
+            print("- Insira a coordenada da peça que você deseja mover -> \"NLinha, NColuna\"")
+            print("- Caso queira sair, digite q")
+            print(f"Turno do jogador {self.turno_jogador}")
 
-        # Alterna o turno
-        self.turno_jogador = 3 - self.turno_jogador
+            pos_valida = False
+            while not pos_valida:
+                ultimo_input = input("Digite as coordenadas (NLinha, NColuna): ")
+                if ultimo_input == "q":
+                    return
+
+                pos_inserida = tuple([int(x) for x in ultimo_input.split(", ")])
+
+                if self.get_tabuleiro(pos_inserida) != self.turno_jogador:
+                    print("A peça selecionada é do outro jogador! Por favor, selecione outra.")
+                else:
+                    pos_valida = True
+
+
+            print("Possíveis direções:")
+            print("e -> esquerda")
+            print("d -> direita")
+            print("ce -> diagonal cima esquerda")
+            print("cd -> diaconal cima direita")
+            print("be -> diagonal baixo esquerda")
+            print("bd -> diagonal baixo direita")
+            ultimo_input = input("Agora, digite a direção: ")
+
+            if ultimo_input == "q":
+                break
+
+            direcao = ultimo_input
+
+            self.movimentar_peca(pos_inserida, direcao)
+
+            # Alterna o turno
+            self.turno_jogador = 3 - self.turno_jogador
